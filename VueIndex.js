24c33cbefa24aa9bbc1822dea0bc564cf0c989c3
WangTZ1996 <2249796929@qq.com>
2019-04-12 17:29:51 +0800

    把掷骰子方法封装成了一个class，并实例化了一个vue组件用来当start按钮。

--- VueIndex.js
+++ VueIndex.js
@@ -1,31 +1,46 @@
-let app1 = new Vue({
-    el: '#app1',
-    data: {
-        msg: '吃点啥呢？',
-        isAction: false,
-    }
-});
+class Dice{
+  roll() {
+    let i = Math.floor(Math.random()*(list.length));
+    app1.isAction = true;
+    setTimeout(function(){
+      app1.msg = list[i].name;
+      console.log(app1.msg,i);
+    },2375);
+    setTimeout(function(){app1.isAction = false;},2500);
+  }
+}
 
-function roll() {
-    let list = [{name : '阿达西',
+let dice = new Dice();
+
+let list = [{name : '阿达西',
                  color : '#629e13'},
                  {name : '南城香',
                  color : '#a81f14'},
                  {name : '大同刀削面',
                  color : '#4364e9'},
-                 {name : '庆丰包子',
-                 color : '#5f210b'},
-                 {name : '鱼你在一起',
-                 color : '#a8f07d'},
                  {name : '护国寺小吃',
                  color : '#3182e7'},
+                 {name : '鱼你在一起',
+                 color : '#a8f07d'},
+                 {name : '南城香',
+                 color : '#f9cae7'},
+                 {name : '护国寺小吃',
+                 color : '#3182e7'},
+                 {name : '南城香',
+                 color : '#f9cae7'},
                  {name : '春饼',
+                 color : '#f9cae7'},
+                 {name : '南城香',
+                 color : '#f9cae7'},
+                 {name : '外卖',
                  color : '#f9cae7'},
                  {name : '阿达西',
                  color : '#629e13'},
                  {name : '南城香',
                  color : '#a81f14'},
-                 {name : '大同刀削面',
+                 {name : '护国寺小吃',
+                 color : '#3182e7'},
+                 {name : '南城香',
                  color : '#4364e9'},
                  {name : '庆丰包子',
                  color : '#5f210b'},
@@ -33,19 +48,30 @@ function roll() {
                  color : '#a8f07d'},
                  {name : '护国寺小吃',
                  color : '#3182e7'},
-                 {name : '春饼',
+                 {name : '南城香',
+                 color : '#f9cae7'},
+                 {name : '外卖',
                  color : '#f9cae7'},
                 ];
-    let i = Math.floor(Math.random()*(list.length));
-    app1.isAction = true;
-    setTimeout(function(){
-        app1.msg = list[i].name;
-    console.log(app1.msg,i);
-    },1600);
-    setTimeout(function(){app1.isAction = false;},2000);
-}
 
-app1.$el.addEventListener('mouseover',roll,false);
+let app1 = new Vue({
+  el: '#app1',
+  data: {
+    msg: '吃点啥呢？',
+    isAction: false,
+  }
+})
+
+let com = new Vue({
+  el: '#component-demo',
+  data: {
+    text: 'START!',
+  }
+})
+
+
+
+
 
 
 
