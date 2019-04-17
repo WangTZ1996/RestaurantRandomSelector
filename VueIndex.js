918bde3cdfc4a825c3fbebbfa1f4dad88351ad5f
WangTZ1996 <2249796929@qq.com>
2019-04-17 17:53:01 +0800

    使用v-for的index实现了input的删除功能，

--- VueIndex.js
+++ VueIndex.js
@@ -3,16 +3,15 @@ new Vue({
   data: {
     restaurantName: '吃点啥呢?',
     isAction: false,
-    items: [
-      { name: '阿达西' },
-      { name: '南城香' },
-      { name: '大同刀削面' },
-      { name: '护国寺小吃' },
-      { name: '鱼你在一起' },
-      { name: '春饼' },
-      { name: '庆丰包子' },
-    ],
     inputMsg: '',
+    items: [
+      { name: '阿达西'},
+      { name: '南城香'},
+      { name: '大同刀削面'},
+      { name: '护国寺小吃'},
+      { name: '鱼你在一起'},
+      { name: '庆丰包子'},
+    ],
   },
   methods: {
     roll: function () {
@@ -22,28 +21,11 @@ new Vue({
       setTimeout(() => this.isAction = false, 1000);
     },
     write: function () {
-      // let name = { name: document.querySelector("#input1").value };
       this.items.unshift({ name: this.inputMsg });
       this.inputMsg = '';
-    }
+    },
+    remove: function (e) {
+      this.items.splice(e.target.innerText.split(".")[0],1);
+    },
   },
 });
-
-
-
-
-
-// 1.1命名修改。
-// 1.2把class逻辑写到vue实例的methods方法。
-// 1.3html标签语意化。
-// 1.4尾逗号不要省略。
-
-
-// 完善需求： input的功能实现。
-
-
-//2.1实现列表的删除。
-//2.2将input功能完全用vue提供的api实现。
-
-
-
