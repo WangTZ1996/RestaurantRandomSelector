12c1cf54e5e1780a96782ebc58b056133b767272
WangTZ1996 <2249796929@qq.com>
2019-04-17 10:41:04 +0800

    将index.js文件改写为只有一个vue根实例，在methods里面使用了箭头函数防止this指向改变。

--- VueIndex.js
+++ VueIndex.js
@@ -1,68 +1,41 @@
-class Dice{
-  roll() {
-    let i = Math.floor(Math.random()*(app1.items.length));
-    app1.isAction = true;
-    setTimeout(function(){
-      app1.msg = app1.items[i].name;
-      console.log(app1.msg,i);
-    },860);
-    setTimeout(function(){app1.isAction = false;},1000);
-  }
-  addRestaurant() {
-    let restaurant = {};
-    restaurant.name = input.inputMsg;
-    app1.items.push(restaurant);
-  }
-}
-
-let dice = new Dice();
-
-// let list = [{name : '阿达西',
-//             color : '#629e13'},
-//             {name : '南城香',
-//             color : '#a81f14'},
-//             {name : '大同刀削面',
-//             color : '#4364e9'},
-//             {name : '护国寺小吃',
-//             color : '#3182e7'},
-//             {name : '鱼你在一起',
-//             color : '#a8f07d'},
-//             {name : '春饼',
-//             color : '#f9cae7'},
-//             {name : '外卖',
-//             color : '#f9cae7'},
-//             {name : '庆丰包子',
-//             color : '#5f210b'},
-//           ];
-
-
-
-let app1 = new Vue({
+new Vue({
   el: '#app1',
   data: {
-      msg: '吃点啥呢?',
-      btn: 'START!',
-      isAction: false,
-      items: [
-        {name : '阿达西'},
-        {name : '南城香'},
-        {name : '大同刀削面'},
-        {name : '护国寺小吃'},
-        {name : '鱼你在一起'},
-        {name : '春饼'},
-        {name : '外卖'},
-        {name : '庆丰包子'},
-      ]
+    restaurantName: '吃点啥呢?',
+    isAction: false,
+    items: [
+      { name: '阿达西' },
+      { name: '南城香' },
+      { name: '大同刀削面' },
+      { name: '护国寺小吃' },
+      { name: '鱼你在一起' },
+      { name: '春饼' },
+      { name: '外卖' },
+      { name: '庆丰包子' },
+    ],
   },
   methods: {
-    play : function() {
-      play: dice.roll()
+    roll: function () {
+      let i = Math.floor(Math.random() * (this.items.length));
+      this.isAction = true; 
+      setTimeout(() =>this.restaurantName = this.items[i].name,800);
+      setTimeout(() =>this.isAction = false,1000);
+    },
+    write: function () {
     }
-  }
+  },
 });
 
 
 
 
 
+
+// 1.命名修改。
+// 2.把class逻辑写到vue实例的methods方法。
+// 3.html标签语意化。
+// 4.尾逗号不要省略。
+
+
+// 完善需求： input的功能实现。
 
