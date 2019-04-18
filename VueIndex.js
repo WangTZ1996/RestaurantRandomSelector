6dbe103b4c51333732818097a936533d02379615
WangTZ1996 <2249796929@qq.com>
2019-04-18 11:18:52 +0800

    实现了输入内容值的校验，并在输入了非法值的时候显示警告效果

--- VueIndex.js
+++ VueIndex.js
@@ -21,19 +21,27 @@ new Vue({
       setTimeout(() => this.isAction = false, 1000);
     },
     write: function () {
-      this.items.unshift({ name: this.inputMsg });
-      this.inputMsg = '';
+      if (this.inputMsg.replace(/\s*/g, "") === "") {
+        document.querySelector("#input1").className = "warning shake";
+        this.inputMsg = '';
+       setTimeout(() => document.querySelector("#input1").className = "warning",1000);
+      } else {
+        this.items.unshift({ name: this.inputMsg });
+        document.querySelector("#input1").className = "";
+        this.inputMsg = '';
+      }
     },
     remove: function (index) {
       this.items.splice(index, 1);
     },
-    hover: function () {
-      let img = document.querySelectorAll("img");
-      img[1].setAttribute("class : act");
-    }
   },
 });
 
 //3.1显示总餐馆数
 //3.2实现显示删除按钮的动态显
 //display:none 和 visibility: hidden区别。
+//输入校验 ，空和空格，检测到非法值并提示（震动，边框变红）并在输入修正后恢复正常样式。
+
+//新需求输入每天吃饭的人数，并且评估每一家餐厅的接待能力排除坐不下的餐厅。
+
+//进阶需求，摇到餐厅并确认以后自动在网上预定座位。
